4f6190496b256f16c29bf9d999872851335aee14
Vivekanand Pujar <vivekanandpv@gmail.com>
2025-11-11 17:23:58 +0530

    publisher service implemented

--- src/main/java/io/caelusresearch/aqrspringbootworkshop/services/PublisherServiceImplementation.java
+++ src/main/java/io/caelusresearch/aqrspringbootworkshop/services/PublisherServiceImplementation.java
@@ -3,34 +3,69 @@ package io.caelusresearch.aqrspringbootworkshop.services;
 import io.caelusresearch.aqrspringbootworkshop.dtos.PublisherCreateRequest;
 import io.caelusresearch.aqrspringbootworkshop.dtos.PublisherResponse;
 import io.caelusresearch.aqrspringbootworkshop.dtos.PublisherUpdateRequest;
+import io.caelusresearch.aqrspringbootworkshop.entities.Publisher;
+import io.caelusresearch.aqrspringbootworkshop.exceptions.RecordNotFoundException;
+import io.caelusresearch.aqrspringbootworkshop.repositories.PublisherRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PublisherServiceImplementation implements PublisherService {
+    private final PublisherRepository publisherRepository;
+
+    public PublisherServiceImplementation(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
     @Override
     public List<PublisherResponse> getAll() {
-        return List.of();
+        return publisherRepository
+                .findAll()
+                .stream()
+                .map(this::toResponse)
+                .toList();
     }
 
     @Override
     public PublisherResponse getById(int PublisherId) {
-        return null;
+        return toResponse(getEntityById(PublisherId));
     }
 
     @Override
     public PublisherResponse create(PublisherCreateRequest request) {
-        return null;
+        return toResponse(publisherRepository.saveAndFlush(toEntity(request)));
     }
 
     @Override
     public PublisherResponse update(int PublisherId, PublisherUpdateRequest request) {
-        return null;
+        Publisher entityDb = getEntityById(PublisherId);
+        
+        BeanUtils.copyProperties(request, entityDb);
+        
+        return toResponse(publisherRepository.saveAndFlush(entityDb));
     }
 
     @Override
     public void deleteById(int PublisherId) {
-
+        publisherRepository.delete(getEntityById(PublisherId));
+    }
+    
+    private PublisherResponse toResponse(Publisher entity) {
+        PublisherResponse response = new PublisherResponse();
+        BeanUtils.copyProperties(entity, response);
+        return response;
+    }
+    
+    private Publisher toEntity(PublisherCreateRequest request) {
+        Publisher entity = new Publisher();
+        BeanUtils.copyProperties(request, entity);
+        return entity;
+    }
+    
+    private Publisher getEntityById(int publisherId) {
+        return publisherRepository.findById(publisherId)
+                .orElseThrow(() -> new RecordNotFoundException("Could not find the publisher: " + publisherId));
     }
 }
