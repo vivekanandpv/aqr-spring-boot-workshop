aa59af61e0a4746aa8a92b375bdf23f932164483
Vivekanand Pujar <vivekanandpv@gmail.com>
2025-11-11 17:34:04 +0530

    author service implemented

--- src/main/java/io/caelusresearch/aqrspringbootworkshop/services/AuthorServiceImplementation.java
+++ src/main/java/io/caelusresearch/aqrspringbootworkshop/services/AuthorServiceImplementation.java
@@ -1,36 +1,71 @@
 package io.caelusresearch.aqrspringbootworkshop.services;
 
-import io.caelusresearch.aqrspringbootworkshop.dtos.AuthorCreateRequest;
-import io.caelusresearch.aqrspringbootworkshop.dtos.AuthorResponse;
-import io.caelusresearch.aqrspringbootworkshop.dtos.AuthorUpdateRequest;
+import io.caelusresearch.aqrspringbootworkshop.dtos.*;
+import io.caelusresearch.aqrspringbootworkshop.entities.Author;
+import io.caelusresearch.aqrspringbootworkshop.entities.Author;
+import io.caelusresearch.aqrspringbootworkshop.exceptions.RecordNotFoundException;
+import io.caelusresearch.aqrspringbootworkshop.repositories.AuthorRepository;
+import io.caelusresearch.aqrspringbootworkshop.repositories.PublisherRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AuthorServiceImplementation implements AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public AuthorServiceImplementation(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
     @Override
     public List<AuthorResponse> getAll() {
-        return List.of();
+        return authorRepository
+                .findAll()
+                .stream()
+                .map(this::toResponse)
+                .toList();
     }
 
     @Override
     public AuthorResponse getById(int AuthorId) {
-        return null;
+        return toResponse(getEntityById(AuthorId));
     }
 
     @Override
     public AuthorResponse create(AuthorCreateRequest request) {
-        return null;
+        return toResponse(authorRepository.saveAndFlush(toEntity(request)));
     }
 
     @Override
     public AuthorResponse update(int AuthorId, AuthorUpdateRequest request) {
-        return null;
+        Author entityDb = getEntityById(AuthorId);
+
+        BeanUtils.copyProperties(request, entityDb);
+
+        return toResponse(authorRepository.saveAndFlush(entityDb));
     }
 
     @Override
     public void deleteById(int AuthorId) {
+        authorRepository.delete(getEntityById(AuthorId));
+    }
 
+    private AuthorResponse toResponse(Author entity) {
+        AuthorResponse response = new AuthorResponse();
+        BeanUtils.copyProperties(entity, response);
+        return response;
+    }
+
+    private Author toEntity(AuthorCreateRequest request) {
+        Author entity = new Author();
+        BeanUtils.copyProperties(request, entity);
+        return entity;
+    }
+
+    private Author getEntityById(int authorId) {
+        return authorRepository.findById(authorId)
+                .orElseThrow(() -> new RecordNotFoundException("Could not find the author: " + authorId));
     }
 }
